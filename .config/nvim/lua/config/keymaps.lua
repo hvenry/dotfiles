8bdbb2ec4942de797fa6aa36c5da964216f4aa9d
hvenry <hvendittelli@gmail.com>
2024-10-26 18:10:22 -0400

    Added tabbing + shift-tabbing in normal / vistual / select modes in Neovim, + new alpha

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -49,3 +49,11 @@ vim.keymap.set(
   "<cmd> TmuxNavigateRight<CR>",
   { noremap = true, silent = true, desc = "Window Right" }
 )
+
+-- Indent current line or selected lines with Tab in Normal and Visual modes
+vim.keymap.set("n", "<Tab>", ">>_", { noremap = true, silent = true })
+vim.keymap.set("v", "<Tab>", ">gv", { noremap = true, silent = true })
+
+-- Outdent current line or selected lines with Shift + Tab in Normal and Visual modes
+vim.keymap.set("n", "<S-Tab>", "<<_", { noremap = true, silent = true })
+vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
