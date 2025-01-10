7f03b51c86002af9df59f0a411fde89c24013c78
hvenry <hvendittelli@gmail.com>
2025-01-09 22:26:33 -0500

    Neovim: neotree now opens to current directory and updated packages

---
+++ .config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    close_if_last_window = true,
+    filesystem = {
+      follow_current_file = true,
+      hijack_netrw_behavior = "open_default",
+      -- show hidden files
+      filtered_items = {
+        visible = true,
+      },
+    },
+  },
+  keys = {
+    -- make 'space + e open' in current direcory
+    {
+      "<Space>e",
+      function()
+        require("neo-tree.command").execute({
+          toggle = true,
+          dir = vim.fn.getcwd(),
+        })
+      end,
+      desc = "Open NeoTree in current directory",
+    },
+  },
+}
