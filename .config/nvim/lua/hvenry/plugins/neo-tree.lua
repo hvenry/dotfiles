d6522b5bcb817666d66716f7680d01c9044d7a85
hvenry <hvendittelli@gmail.com>
2025-04-03 16:27:53 -0400

    Neovim: updated neotree config

--- .config/nvim/lua/hvenry/plugins/neo-tree.lua
+++ .config/nvim/lua/hvenry/plugins/neo-tree.lua
@@ -1,6 +1,12 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+    "3rd/image.nvim",
+  },
   config = function()
     require("neo-tree").setup({
       close_if_last_window = true,
@@ -9,50 +15,82 @@ return {
       enable_diagnostics = true,
       filesystem = {
         follow_current_file = { enabled = true },
+        hijack_netrw_behavior = "open_default",
         use_libuv_file_watcher = true,
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_hidden = false,
+          hide_by_name = {
+            ".git",
+            "node_modules",
+          },
+          never_show = {
+            ".DS_Store",
+            "Thumbs.db",
+          },
+        },
       },
       window = {
-        position = "right", -- "float"
+        position = "right",
         width = 30,
         mappings = {
           ["<CR>"] = "open",
+          ["<bs>"] = "navigate_up",
+          ["."] = "set_root",
+          ["H"] = "toggle_hidden",
+          ["/"] = { "fuzzy_finder", config = { title = "Filter" } },
+          ["f"] = "filter_on_submit",
+          ["s"] = "open_vsplit",
+          ["S"] = "open_split",
         },
-        preserve_window_proportions = true, -- prevents window size changes
+        preserve_window_proportions = true,
       },
       buffers = {
-        follow_current_file = { enabled = true }, -- auto-focus buffer you navigate to
+        follow_current_file = { enabled = true },
         group_empty_dirs = true,
       },
       event_handlers = {
         {
           event = "neo_tree_buffer_enter",
-          handler = function()
+          handler = function(arg)
             vim.opt_local.signcolumn = "auto"
+            vim.opt_local.winbar = "%#Directory# [ File Explorer ] %*"
           end,
+        },
+      },
+      default_component_configs = {
+        indent = {
+          indent_size = 2,
+          padding = 1,
+          with_markers = true,
+          indent_marker = "│",
+          last_indent_marker = "└",
+          highlight = "NeoTreeIndentMarker",
+        },
+        git_status = {
+          symbols = {
+            added = "✚",
+            modified = "",
+            deleted = "✖",
+            renamed = "➜",
+            untracked = "★",
+            ignored = "◌",
+            unstaged = "✗",
+            staged = "✓",
+            conflict = "",
+          },
         },
       },
     })
 
-    -- Toggle Neo-tree
-    vim.api.nvim_set_keymap("n", "<leader>e", ":Neotree toggle<CR>", { noremap = true, silent = true })
-
-    -- Auto-open Neo-tree when starting Neovim with a directory argument
-    vim.api.nvim_create_autocmd("VimEnter", {
-      callback = function(data)
-        -- Check if a directory was passed as an argument
-        local directory = vim.fn.isdirectory(data.file) == 1
-
-        if directory then
-          -- Change to the directory
-          vim.cmd.cd(data.file)
-
-          -- Open Neo-tree automatically
-          vim.cmd("Neotree show")
-
-          -- Clear the unnamed buffer
-          vim.cmd("bd " .. data.buf)
-        end
-      end,
-    })
+    -- open with space e
+    vim.api.nvim_set_keymap(
+      "n",
+      "<leader>e",
+      ":Neotree toggle<CR>",
+      { noremap = true, silent = true, desc = "Explorer NeoTree toggle" }
+    )
   end,
 }
