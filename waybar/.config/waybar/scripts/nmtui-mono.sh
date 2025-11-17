658f32c6f2aa0948e9479f368855793a7aca042e
Henry Vendittelli <hvendittelli@gmail.com>
2025-11-17 17:59:19 -0500

    waybar: added custom nmtui popup, mute/unmute, wlogout integration and some extra tweaks

---
+++ waybar/.config/waybar/scripts/nmtui-mono.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+export NEWT_COLORS='
+root=white,black
+border=white,black
+window=white,black
+shadow=,black
+title=white,black
+button=black,white
+actbutton=white,black
+checkbox=white,black
+actcheckbox=black,white
+entry=black,white
+label=white,black
+listbox=white,black
+actlistbox=black,white
+textbox=white,black
+acttextbox=black,white
+helpline=white,black
+roottext=white,black
+emptyscale=,black
+fullscale=,white
+disentry=gray,black
+compactbutton=white,black
+actsellistbox=black,white
+sellistbox=white,black
+'
+exec nmtui
